cf3719078bce97a0a26fda011d6071cb3f40ec74
Tyler Garner <garnertb@gmail.com>
2022-12-17 16:26:46 -0500

    Format Go code.  Add vscode task to format.

--- cmd/root.go
+++ cmd/root.go
@@ -5,17 +5,17 @@ Copyright © 2022 Tyler Garner garnertb@github.com
 package cmd
 
 import (
-	"os"
+	_ "embed"
+	b64 "encoding/base64"
+	"encoding/json"
 	"fmt"
 	"github.com/spf13/cobra"
-	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"io/ioutil"
-	"time"
-	b64 "encoding/base64"
+	"os"
 	"strings"
-	_ "embed"
+	"time"
 )
 
 //go:embed gitignore.json
@@ -23,35 +23,35 @@ var fileContent string
 
 type Files struct {
 	Tree []File `json:"tree"`
-	Sha string `json:"sha"`
+	Sha  string `json:"sha"`
 }
 
 type File struct {
-	Path string `json:"path"`
-	Mode string `json:"mode"`
+	Path     string `json:"path"`
+	Mode     string `json:"mode"`
 	Filetype string `json:"type"`
-	Sha string `json:"sha"`
-	Url string `json:"url"`
+	Sha      string `json:"sha"`
+	Url      string `json:"url"`
 }
 
 type FileContent struct {
-	Sha string `json:"sha"`
-	NodeId string `json:"node_id"`
-	Size int `json:"size"`
-	Url string `json:"url"`
-	Content string `json:"content"`
+	Sha      string `json:"sha"`
+	NodeId   string `json:"node_id"`
+	Size     int    `json:"size"`
+	Url      string `json:"url"`
+	Content  string `json:"content"`
 	Encoding string `json:"encoding"`
 }
 
 type Commands map[string][]string
 
 var rootCmd = &cobra.Command{
-	Use:   "gh-gitignore",
-	Short: "Load gitignore files from GitHub into your project",
-	Long: `Load gitignore files from GitHub into your project`,
+	Use:       "gh-gitignore",
+	Short:     "Load gitignore files from GitHub into your project",
+	Long:      `Load gitignore files from GitHub into your project`,
 	ValidArgs: []string{"node", "go"},
-	Args:  cobra.ExactValidArgs(1),
-	Example: "gh-gitignore node",
+	Args:      cobra.ExactValidArgs(1),
+	Example:   "gh-gitignore node",
 	Run: func(cmd *cobra.Command, args []string) {
 		var filetype = args[0]
 
@@ -65,43 +65,43 @@ var rootCmd = &cobra.Command{
 
 		var Dec, _ = b64.StdEncoding.DecodeString(res.Content)
 		fmt.Println(string(Dec))
-	 },
+	},
 }
 
 var myClient = &http.Client{Timeout: 10 * time.Second}
 
 func getJson(url string, target interface{}) error {
-    r, err := myClient.Get(url)
-    if err != nil {
-				log.Fatal(err)
-        return err
-    }
-    var body, error = ioutil.ReadAll(r.Body)
-		
-		if error != nil {
-			log.Fatal(error)
-			return error
-		}
+	r, err := myClient.Get(url)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	var body, error = ioutil.ReadAll(r.Body)
 
-    return json.Unmarshal(body, &target)
+	if error != nil {
+		log.Fatal(error)
+		return error
+	}
+
+	return json.Unmarshal(body, &target)
 }
 
-func ReadFile()(Commands) {
-	 var files Files
-   json.Unmarshal([]byte(fileContent), &files)
+func ReadFile() Commands {
+	var files Files
+	json.Unmarshal([]byte(fileContent), &files)
 
-	 filteredCommands := make(map[string][]string)
+	filteredCommands := make(map[string][]string)
 
-	 for i := 0; i < len(files.Tree); i++ {
+	for i := 0; i < len(files.Tree); i++ {
 		var file = files.Tree[i]
 
-		if (strings.Contains(file.Path, ".gitignore")) {
+		if strings.Contains(file.Path, ".gitignore") {
 			command := strings.ToLower(strings.Split(file.Path, ".gitignore")[0])
 			filteredCommands[command] = append(filteredCommands[command], file.Url)
 		}
-	 }
+	}
 
-	 return filteredCommands
+	return filteredCommands
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -125,5 +125,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
